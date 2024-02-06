840acb6d1b4f1a3397d449305f1c57bfc0a877e8
fabien <flierville@cesi.fr>
2024-02-06 11:52:01 +0100

    Location

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:location/location.dart';
+import 'package:meteo/models/device_info.dart';
 import 'package:meteo/pages/home_page.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  Location location = new Location();
+
+  bool _serviceEnabled;
+  PermissionStatus _permissionGranted;
+  LocationData _locationData;
+
+  _serviceEnabled = await location.serviceEnabled();
+  if (!_serviceEnabled) {
+    _serviceEnabled = await location.requestService();
+    if (!_serviceEnabled) {
+      return;
+    }
+  }
+
+  _permissionGranted = await location.hasPermission();
+  if (_permissionGranted == PermissionStatus.denied) {
+    _permissionGranted = await location.requestPermission();
+    if (_permissionGranted != PermissionStatus.granted) {
+      return;
+    }
+  }
+
+  _locationData = await location.getLocation();
+  DeviceInfo.longitude = _locationData.longitude;
+  DeviceInfo.latitude = _locationData.latitude;
+
   runApp(const MyApp());
 }
 
---
+++ lib/models/device_info.dart
@@ -0,0 +1,5 @@
+class DeviceInfo{
+  static String? ville;
+  static double? latitude;
+  static double? longitude;
+}
